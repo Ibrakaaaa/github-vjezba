0c591afe20a34e3938a39d4208310e25f8d9f8ba
Ibrahim Merkez <ibrahim.merkezlol@gmail.com>
2025-09-30 23:28:51 +0200

    dodao Testing funkciju

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [advice, setAdvice] = useState('');
+  const [count, setCount] = useState(0)
+
+  async function fetchAdvice() {
+    try {
+      const res = await fetch('https://api.adviceslip.com/advice');
+      const data = await res.json();
+      console.log(data);
+      setAdvice(data.slip.advice); // ovde čuvamo savet u state
+    } catch {
+      console.log('error');
+      throw new Error('Failed to fetch advice');
+    }
+    setCount(count + 1)
+  }
+
+  useEffect(() => {
+
+    fetchAdvice()
+
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={fetchAdvice}>Click me</button>
+      <Message count={count} />
+<Testing />
     </div>
   );
+}
+
+
+function Message({count}) {
+  return <p>You have read in total <strong>{count}</strong> advices </p>
+
+}
+
+function Testing() {
+  return <h1>Testing Git</h1>
 }
 
 export default App;
